e128ac4c8170af11a56ab5f363775214659a680e
Dinesh Kumar Meher <dineshmeher641@gmail.com>
2022-12-27 20:19:28 +0530

    sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum{
     public static void main(String[] args) {
       int  a=8;
       int  b=9;
-      System.out.println(a+b);
+      System.out.println("sum = " +(a+b));
     }
 }
